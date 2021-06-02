db2c9a9e0b48675d40d4c55e1cde920ae38c286b
Danny Bastos <danny.bastos.br@gmail.com>
2021-06-02 11:39:01 -0300

    added content-type to response object

--- src/main/java/com/example/jaxrsdemo/HelloController.java
+++ src/main/java/com/example/jaxrsdemo/HelloController.java
@@ -2,13 +2,16 @@ package com.example.jaxrsdemo;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public interface HelloController {
 
-    @Path("/")
     @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_PLAIN)
     Response sayHello(@QueryParam("name") String name);
 }
